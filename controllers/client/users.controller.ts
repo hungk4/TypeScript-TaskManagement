d1891ecc3ff7eb1c4d99a84ed6ef5ebf8b3417c9
hungk4 <nguyenmanhhung15022004@gmail.com>
2024-09-12 17:16:03 +0700

    tinh nag dang nhap

--- controllers/client/users.controller.ts
+++ controllers/client/users.controller.ts
@@ -49,3 +49,43 @@ export const register = async (req: Request, res: Response) => {
 }
 
 
+// [POST] /users/login
+export const login = async (req: Request, res: Response) => {
+  try{
+    const email = req.body.email;
+    const password = md5(req.body.password);
+    const user = await User.findOne({
+      email: email,
+      deleted: false
+    });
+
+    if(!user){
+      res.json({
+        code: 400,
+        message: "Không tồn tại email trong hệ thống"
+      });
+      return;
+    }
+
+    if(user.password !== password){
+      res.json({
+        code: 400,
+        message: "Sai mật khẩu!"
+      });
+      return;
+    }
+
+    res.json({
+      code: 200,
+      token: user.token,
+      message: "Đăng nhập thành công!"
+    })
+    
+  } catch(e){
+    res.json({
+      code: 400,
+      message: "Not Found"
+    })
+  }
+}
+
